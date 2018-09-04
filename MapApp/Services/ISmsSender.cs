b3c8acebf223b4197252c5e921c9ff1cd6d8edd1
Ilay Pilosof <ilay.pilo@cs.colman.ac.il>
2018-09-04 19:45:19 +0300

    Added user authentication

---
+++ MapApp/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapApp.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
