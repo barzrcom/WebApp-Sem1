487b592670005bddde8c7b6d7c66d5833f399ec3
Bar Zrihan <barzrcom@cs.colman.ac.il>
2018-09-10 11:05:32 +0300

    Location Model implemention and DB migration

---
+++ MapApp/Data/Migrations/20180910075545_location.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MapApp.Data.Migrations
+{
+    public partial class location : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CloseTime = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    District = table.Column<string>(nullable: true),
+                    Duration = table.Column<float>(nullable: false),
+                    LastVisit = table.Column<DateTime>(nullable: false),
+                    Latitude = table.Column<long>(nullable: false),
+                    Longitude = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    OpenTime = table.Column<DateTime>(nullable: false),
+                    Seasons = table.Column<byte>(nullable: false),
+                    Type = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
---
+++ MapApp/Models/LocationModels/Location.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public enum LocationType : byte
+{ Type1, Type2 }
+
+public enum Seasons : byte
+{ Winter, Spring, Summer, Autumn }
+
+namespace MapApp.Models.LocationModels
+{
+
+	public class Location
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public LocationType Type { get; set; }
+
+		public long Latitude { get; set; }
+
+		public long Longitude { get; set; }
+
+		public string District { get; set; }
+
+		public Seasons Seasons { get; set; }
+
+		public DateTime OpenTime { get; set; }
+
+		public DateTime CloseTime { get; set; }
+
+		public DateTime LastVisit { get; set; }
+
+		public float Duration { get; set; }  // Duration in Hours
+	}
+}
