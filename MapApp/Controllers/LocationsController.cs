45d1333d18c593d279e9ecc18ce62efff7609c8d
Bar Zrihan <barzrcom@cs.colman.ac.il>
2018-09-14 14:42:26 +0300

    Get Image at location view

--- MapApp/Controllers/LocationsController.cs
+++ MapApp/Controllers/LocationsController.cs
@@ -9,31 +9,32 @@ using MapApp.Models;
 using MapApp.Models.LocationModels;
 using MapApp.Data;
 using Microsoft.AspNetCore.Authorization;
+using System.IO;
 
 namespace MapApp.Controllers
 {
-    public class LocationsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	public class LocationsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public LocationsController(ApplicationDbContext context)
-        {
-            _context = context;    
-        }
+		public LocationsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
 		// GET: Locations
 		public async Task<IActionResult> Index()
-        {			
-            return View(await _context.Location.ToListAsync());
-        }
+		{
+			return View(await _context.Location.ToListAsync());
+		}
 
-        // GET: Locations
-        public async Task<IActionResult> Data()
-        {
-            return Json(await _context.Location.ToListAsync());
-        }
+		// GET: Locations
+		public async Task<IActionResult> Data()
+		{
+			return Json(await _context.Location.ToListAsync());
+		}
 
-        [Authorize]
+		[Authorize]
 		// GET: MyLocationsIndex
 		public async Task<IActionResult> MyLocationsIndex()
 		{
@@ -43,135 +44,151 @@ namespace MapApp.Controllers
 		[Authorize]
 		// GET: Locations/Details/5
 		public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var location = await _context.Location
-                .SingleOrDefaultAsync(m => m.ID == id);
-            if (location == null)
-            {
-                return NotFound();
-            }
+			var location = await _context.Location
+				.SingleOrDefaultAsync(m => m.ID == id);
+			if (location == null)
+			{
+				return NotFound();
+			}
 
-            return View(location);
-        }
+			return View(location);
+		}
 
 		[Authorize]
 		// GET: Locations/Create
 		public IActionResult Create()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 
 		[Authorize]
 		// POST: Locations/Create
 		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Description,Category,Latitude,Longitude,Image")] Location location)
-        {
-            if (ModelState.IsValid)
-            {
-				location.User = User.Identity.Name;
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("ID,Name,Description,Category,Latitude,Longitude")] Location location, IFormFile Image)
+		{
+			if (ModelState.IsValid)
+			{
+				this.UpdateCustomFields(location, Image);
 				_context.Add(location);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(location);
-        }
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(location);
+		}
 
 		[Authorize]
 		// GET: Locations/Edit/5
 		public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var location = await _context.Location.SingleOrDefaultAsync(m => m.ID == id);
-            if (location == null)
-            {
-                return NotFound();
-            }
-            return View(location);
-        }
+			var location = await _context.Location.SingleOrDefaultAsync(m => m.ID == id);
+			if (location == null)
+			{
+				return NotFound();
+			}
+			return View(location);
+		}
 
 		[Authorize]
 		// POST: Locations/Edit/5
 		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Description,Category,Latitude,Longitude,Image")] Location location)
-        {
-            if (id != location.ID)
-            {
-                return NotFound();
-            }
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Description,Category,Latitude,Longitude")] Location location, IFormFile Image)
+		{
+			if (id != location.ID)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-					location.User = User.Identity.Name;
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					this.UpdateCustomFields(location, Image);
 					_context.Update(location);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!LocationExists(location.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            return View(location);
-        }
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!LocationExists(location.ID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction("Index");
+			}
+			return View(location);
+		}
 
 		[Authorize]
 		// GET: Locations/Delete/5
 		public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var location = await _context.Location
-                .SingleOrDefaultAsync(m => m.ID == id);
-            if (location == null)
-            {
-                return NotFound();
-            }
+			var location = await _context.Location
+				.SingleOrDefaultAsync(m => m.ID == id);
+			if (location == null)
+			{
+				return NotFound();
+			}
 
-            return View(location);
-        }
+			return View(location);
+		}
 
 		[Authorize]
 		// POST: Locations/Delete/5
 		[HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var location = await _context.Location.SingleOrDefaultAsync(m => m.ID == id);
-            _context.Location.Remove(location);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var location = await _context.Location.SingleOrDefaultAsync(m => m.ID == id);
+			_context.Location.Remove(location);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
 
-        private bool LocationExists(int id)
-        {
-            return _context.Location.Any(e => e.ID == id);
-        }
-    }
+		private bool LocationExists(int id)
+		{
+			return _context.Location.Any(e => e.ID == id);
+		}
+
+		private async void UpdateCustomFields(Location location, IFormFile Image)
+		{
+			// TODO: Convert Image file to byte here.
+			if (Image == null || Image.Length > 0)
+			{
+				using (var stream = new MemoryStream())
+				{
+					await Image.CopyToAsync(stream);
+					location.Image = stream.ToArray();
+				}
+			}
+
+			//location.Image = Convert.ToBase64String(location.Image);
+			location.User = User.Identity.Name;
+		}
+	}
 }
