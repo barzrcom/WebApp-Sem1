9a032024b8c41911920eebc32fd94783e13a618a
ilay.pilo <ilay.pilo@gmail.com>
2018-09-14 14:30:32 +0300

    Added Roles and admin panel

---
+++ MapApp/Controllers/AdminController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MapApp.Models;
+using MapApp.Models.LocationModels;
+using MapApp.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MapApp.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: Locations
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/Users
+        public async Task<IActionResult> Users()
+        {
+            
+            return View(await _context.Users.ToListAsync());
+        }
+
+        [Authorize]
+        // GET: Locations/Delete/5
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [Authorize]
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("DeleteUser")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUserConfirmed(string id)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Users");
+        }
+    }
+}
--- MapApp/Startup.cs
+++ MapApp/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using MapApp.Data;
 using MapApp.Models;
 using MapApp.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace MapApp
 {
@@ -53,9 +54,37 @@ namespace MapApp
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
         }
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            IdentityResult roleResult;
+            //Adding Admin Role
+            var roleCheck = await RoleManager.RoleExistsAsync("Administrator");
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Administrator"));
+            }
+            roleCheck = await RoleManager.RoleExistsAsync("User");
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("User"));
+            }
+            //Assign Admin role to the main User here we have given our newly registered 
+            //login id for Admin management
+            ApplicationUser user = await UserManager.FindByEmailAsync("admin@admin.com");
+            if (user != null)
+            {
+                roleResult = await UserManager.AddToRoleAsync(user, "Administrator");
+            }
+            
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider services)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -83,6 +112,8 @@ namespace MapApp
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            CreateUserRoles(services).Wait();
         }
     }
 }
