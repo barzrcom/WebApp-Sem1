951f33676b411c93458862298b4591e35baef63a
Bar Zrihan <barzrcom@cs.colman.ac.il>
2018-09-14 13:03:29 +0300

    Minimized Location model fields

---
+++ MapApp/Data/Migrations/20180914094907_location_remove_fields.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MapApp.Data.Migrations
+{
+    public partial class location_remove_fields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CloseTime",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "District",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "LastVisit",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "OpenTime",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "Seasons",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Location");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Category",
+                table: "Location",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "Location",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Location");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CloseTime",
+                table: "Location",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "District",
+                table: "Location",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Duration",
+                table: "Location",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastVisit",
+                table: "Location",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OpenTime",
+                table: "Location",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Seasons",
+                table: "Location",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Type",
+                table: "Location",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
--- MapApp/Models/LocationModels/Location.cs
+++ MapApp/Models/LocationModels/Location.cs
@@ -5,11 +5,9 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-public enum LocationType : byte
-{ Type1, Type2 }
+public enum LocationCategory : byte
+{ ViewPoint, Restaurant, Museum }
 
-public enum Seasons : byte
-{ Winter, Spring, Summer, Autumn }
 
 namespace MapApp.Models.LocationModels
 {
@@ -24,28 +22,12 @@ namespace MapApp.Models.LocationModels
 
 		public string Description { get; set; }
 
-		public LocationType Type { get; set; }
+		public LocationCategory Category { get; set; }
 
 		public float Latitude { get; set; }
 
 		public float Longitude { get; set; }
 
-		public string District { get; set; }
-
-		public Seasons Seasons { get; set; }
-
-		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:MM}")]
-		[DisplayName("Opening Time")]
-		public DateTime OpenTime { get; set; }
-
-		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:MM}")]
-		[DisplayName("Closing Time")]
-		public DateTime CloseTime { get; set; }
-
-		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
-		[DisplayName("Visit Date")]
-		public DateTime LastVisit { get; set; }
-
-		public float Duration { get; set; }  // Duration in Hours
+		public byte[] Image { get; set; }
 	}
 }
