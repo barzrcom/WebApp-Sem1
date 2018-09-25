7ead4de9694f6b3a6e008c46c2d5313a312309dd
liortzur <43174315+liortzur@users.noreply.github.com>
2018-09-26 02:05:30 +0300

    Rating support added. Location and Comment model has changed. View updated where needed. Rating calculation added when viewing location details (happens automatically after comment created, deleted or edited).

---
+++ MapApp/Data/Migrations/20180925194608_added_rating_comment_location.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MapApp.Data.Migrations
+{
+    public partial class added_rating_comment_location : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Location",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Header",
+                table: "Comment",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comment",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Comment",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Comment");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Header",
+                table: "Comment",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comment",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
