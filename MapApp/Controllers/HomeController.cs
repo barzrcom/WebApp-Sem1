a21478a09716c2979bc9c1761a16de93bbf7dd5b
Amir Sasson <amirs91@gmail.com>
2018-10-20 20:45:20 +0300

    home controller fix

--- MapApp/Controllers/HomeController.cs
+++ MapApp/Controllers/HomeController.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MapApp.Data;
 
 namespace MapApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public HomeController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
         public IActionResult Index()
         {
             return View();
